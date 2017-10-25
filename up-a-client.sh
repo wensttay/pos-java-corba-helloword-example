ff1be5d0a721f57e05b1c06a23f838595c69785c
wensttay <yattsnew@gmail.com>
2017-10-25 19:17:42 -0300

    Add up-a-client.sh command

---
+++ up-a-client.sh
@@ -0,0 +1,1 @@
+java HelloClient -ORBInitialPort 1050 -ORBInitialHost localhost&
--- up.sh
+++ up.sh
@@ -1,5 +1,5 @@
 idlj -fall Hello.idl
 javac *.java HelloApp/*.java
 orbd -ORBInitialPort 1050 -ORBInitialHost localhost&
-sleep 5
+sleep 3
 java HelloServer -ORBInitialPort 1050 -ORBInitialHost localhost&
